33a8f73406afd9689526feb1fe0ff28445dc5ebc
Luis de Diego Toro <luis.de.external@atos.net>
2019-03-30 00:11:52 +0100

    -[MVC] Testing another initialisation by using the DataSourceManager. Has some defects and not working on production mode.

--- AndroidMVCLibrary/src/main/java/org/dimensinfin/android/mvc/datasource/DataSourceManager.java
+++ AndroidMVCLibrary/src/main/java/org/dimensinfin/android/mvc/datasource/DataSourceManager.java
@@ -1,69 +1,80 @@
 package org.dimensinfin.android.mvc.datasource;
 
-import java.util.HashMap;
-import java.util.concurrent.Future;
-
+import org.dimensinfin.android.mvc.activity.AbstractPagerFragment;
 import org.dimensinfin.android.mvc.core.AppCompatibilityUtils;
-
-import androidx.annotation.NonNull;
+import org.dimensinfin.android.mvc.core.EEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.beans.PropertyChangeEvent;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
- * Controls and caches all DataSources in use that are allowed to be cacheable. Will use a single multifield Locator to
+ * Controls and caches all DataSources in use that are allowed to be cached. Will use a single multi field Locator to
  * store and remember used DataSources and their state.
  *
+ * The right implementation will use the knowledge of caching to call the data initialization entry point only when
+ * there is a new data source to be registered. With that approach there should not be qny control for duplication and
+ * caching outside the single class that knows about that feature that id this <code>DataSourceManager</code>.
  * @author Adam Antinoo
  */
 public class DataSourceManager {
 	// - S T A T I C - S E C T I O N
-	private static Logger logger = LoggerFactory.getLogger("DataSourceManager");
+	private static Logger logger = LoggerFactory.getLogger(DataSourceManager.class);
+	private static final ExecutorService _bkgndExecutor = Executors.newFixedThreadPool(1);
 	private static final HashMap<String, IDataSource> dataSources = new HashMap<String, IDataSource>();
 
 	// - M E T H O D - S E C T I O N
 
 	/**
 	 * Registers this new DataSource on the Manager or returns the source located already on the cache if they unique
-	 * identifiers match. This way I will get a cached and already prepared DataSource if I try to create another with the
-	 * same identifier.
-	 *
-	 * @param newSource new DataSource to add to the Manager
+	 * identifiers match. This way I will get a cached and already prepared DataSource if I try to create another with
+	 * the same identifier. If the data source if not found on the cache of the cache falg says it should not be cached
+	 * then the manager should initialize the data source data structures and call the UI render updated when that model
+	 * construction is finished.
+	 * @param newSource     new DataSource to add to the Manager
+	 * @param pagerFragment the fragment where this ds is registered and used. This parameter is need to signal the
+	 *                      removal from the display of the spinner progress indicator
 	 * @return the oldest DataSource with the same identifier.
 	 */
-	public static IDataSource registerDataSource(@NonNull final IDataSource newSource) {
-		//		if (null == newSource) return newSource;
-		AppCompatibilityUtils.parameterNotNull(newSource);
+	public static IDataSource registerDataSource( final IDataSource newSource, final AbstractPagerFragment pagerFragment ) {
+		AppCompatibilityUtils.testAssertNotNull(newSource);
+//		if (null == newSource) return newSource;
 		// Check if the data source can be cached.
-		if (newSource.needsCaching()) {
+		if (newSource.isCacheable()) {
 			DataSourceLocator locator = newSource.getDataSourceLocator();
 			// Search for locator on cache.
-			final IDataSource found = DataSourceManager.dataSources.get(locator.getIdentity());
-			if (null == found) {
+			IDataSource found = DataSourceManager.dataSources.get(locator.getIdentity());
+			if (null == found) { // New data source. Register and initialize
 				DataSourceManager.dataSources.put(locator.getIdentity(), newSource);
 				DataSourceManager.logger
-						.info("-- [DataSourceManager.registerDataSource]> Registering new DataSource: {}", locator.getIdentity());
-				//				found = newSource;
-				AppCompatibilityUtils.backgroundExecutor.submit(() -> {
-					try {
-						newSource.prepareModel();
-					} catch (RuntimeException runtime) {
-						runtime.printStackTrace();
-					}
+						.info("-- [DataSourceManager.registerDataSource]> Registering new DataSource: " + locator.getIdentity());
+				return initializeDS(newSource, pagerFragment);
+			} else {
+				// Force the model update.
+				_bkgndExecutor.submit(() -> {
+//					DataSourceManager.logger
+//							.info("-- [DataSourceManager.registerDataSource]> Initialising DataSource: " + ds.getDataSourceLocator().getIdentity());
+//					ds.collaborate2Model(); // Create ds model.
+					found.propertyChange(new PropertyChangeEvent(found, EEvents.EVENTCONTENTS_ACTIONMODIFYDATA.name(), null, null));
+					pagerFragment.hideProgressIndicator(); // Hide the progress spinner.
 				});
-				//				newSource.setSynchronizer(sync);
-				//				return newSource;
-			} else
-				// Request to start the model preparation.
-				AppCompatibilityUtils.backgroundExecutor.submit(() -> {
-					try {
-						found.prepareModel();
-					} catch (RuntimeException runtime) {
-						runtime.printStackTrace();
-					}
-				});
-			//			else
-			//				newSource= found;
-		}
-		return newSource;
+				return found;
+			}
+		} else return initializeDS(newSource, pagerFragment);
+//		return newSource;
+	}
+
+	private static IDataSource initializeDS( final IDataSource ds, final AbstractPagerFragment pagerFragment ) {
+		_bkgndExecutor.submit(() -> {
+			DataSourceManager.logger
+					.info("-- [DataSourceManager.registerDataSource]> Initialising DataSource: " + ds.getDataSourceLocator().getIdentity());
+			ds.collaborate2Model(); // Create ds model.
+			ds.propertyChange(new PropertyChangeEvent(ds, EEvents.EVENTCONTENTS_ACTIONMODIFYDATA.name(), null, null));
+			pagerFragment.hideProgressIndicator(); // Hide the progress spinner.
+		});
+		return ds;
 	}
 }
--- AndroidMVCLibrary/src/main/java/org/dimensinfin/android/mvc/datasource/IDataSource.java
+++ AndroidMVCLibrary/src/main/java/org/dimensinfin/android/mvc/datasource/IDataSource.java
@@ -1,45 +1,36 @@
 package org.dimensinfin.android.mvc.datasource;
 
+import org.dimensinfin.android.mvc.controller.IAndroidController;
+
 import java.beans.PropertyChangeListener;
 import java.util.List;
-import java.util.concurrent.Future;
-
-import org.dimensinfin.android.mvc.controller.IAndroidController;
 
 public interface IDataSource extends PropertyChangeListener {
 	DataSourceLocator getDataSourceLocator();
 
-	boolean needsCaching();
-
+	boolean isCacheable();
 
 	void addPropertyChangeListener(final PropertyChangeListener newListener);
 
 	/**
-	 * This is the action to signal the data source to start searching for its data to process it and generate
-	 * the contents for the root model list. The model generated is still not processed and no controllers are created until
-	 * the viewer requires them. This call is to create any internal data source data structures that are to be used when extracting the
-	 * header and data section models.
+	 * This is the action to signal the data source to start searching for its data to process it and generate the
+	 * contents for the root model list. The model generated is still not processed and no controllers are created until
+	 * the viewer requires them. This call is to create any internal data source data structures that are to be used when
+	 * extracting the header and data section models.
 	 */
 	void prepareModel();
 
 	/**
-	 * With this method the initializer will set a synchronization trap that will force any caller that requires the model contents to
-	 * wait until the initialisation is completed and the model root list is populated.
-	 */
-//	void setSynchronizer(final Future<IDataSource> synchronizer);
-
-	/**
-	 * This method will use the data source models to generate the contents for the header section. The model will be transformed to the list of
-	 * Controllers so the final header view will have the list of render views to be used on the display.
-	 *
+	 * This method will use the data source models to generate the contents for the header section. The model will be
+	 * transformed to the list of Controllers so the final header view will have the list of render views to be used on
+	 * the display.
 	 * @return the list of header controllers to be rendered.
 	 */
 	List<IAndroidController> getHeaderSectionContents();
 
 	/**
-	 * This is the method to generate the list of controllers to be used on the list rendering. The list returned is only for the controllers that are
-	 * visible and then that collaborate to the view.
-	 *
+	 * This is the method to generate the list of controllers to be used on the list rendering. The list returned is only
+	 * for the controllers that are visible and then that collaborate to the view.
 	 * @return the list of data section controllers to be rendered.
 	 */
 	List<IAndroidController> getDataSectionContents();
